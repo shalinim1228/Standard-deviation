192f7a07b1750d1bf293f54caea1f89b4f2433bb
SHALINI.M <shalum0602@gmail.com>
2024-08-14 22:09:04 +0530

    Add files via upload
    This project is a simple Python script that demonstrates how to calculate basic statistical measures using the statistics module. The script computes the mean, variance, standard deviation, and median for a predefined dataset.
    
    Features:
    Mean Calculation: Computes the average value of the dataset.
    Variance Calculation: Computes the variance, which measures the spread of the data points.
    Standard Deviation Calculation: Computes the standard deviation, which indicates the amount of variation or dispersion of the data points.
    Median Calculation: Computes the median, which is the middle value in the sorted dataset.

---
+++ STAT.py
@@ -0,0 +1,20 @@
+import math as mt
+
+import statistics as st
+
+data=[1,2,3,4,5]
+
+a=st.mean(data)
+
+print(a)
+
+b=st.variance(data)
+print(b)
+
+c=st.stdev(data)
+
+print(c)
+
+d=st.median(data)
+
+print(d)
